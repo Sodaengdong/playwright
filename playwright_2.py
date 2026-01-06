847b23e122d207ec7e88d1a3e3790b9210b5742d
Sodaengdong <oisujin98@gmail.com>
2026-01-06 16:47:12 +0900

    commit2

--- playwright_2.py
+++ playwright_2.py
@@ -1,10 +1,12 @@
+# launch 메서드에 headless=True 옵션 사용하여 브라우저 백그라운드에서 실행하기
+
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 # Playwright 실행
 
 with sync_playwright() as p :
     # Chromium 브라우저 시작
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True) # headless = True가 기본값이라 작성하지 않아도 됨, 브라우저 창 뜨지 않고 백그라운드에서만 실행됨 
     page = browser.new_page()
     
     
@@ -14,5 +16,3 @@ with sync_playwright() as p :
     
     # 브라우저 종료
     browser.close()
-    
-        
