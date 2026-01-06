a3c2d97ea30b5d92d24d6f67091c96971ef22ff1
Sodaengdong <oisujin98@gmail.com>
2026-01-06 16:56:06 +0900

    commit3

---
+++ playwright_3.py
@@ -0,0 +1,19 @@
+#Firefox로 브라우저로 실행하기
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+# Playwright 실행
+
+with sync_playwright() as p :
+    # Firefox 브라우저 시작
+    browser = p.firefox.launch(headless=False, slow_mo=100) # slow_mo : 각 Playwright 동작 사이마다 100밀리초(ms)씩 지연, 눈으로 확인 가능
+    page = browser.new_page()
+    
+    
+    # 페이지 이동
+    page.goto("https://www.google.com/")
+    print("Page Title : ", page.title()) # 페이지 제목 출력
+    
+    # 브라우저 종료
+    browser.close()
+    
