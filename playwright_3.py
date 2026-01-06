3d9ceed709e79ddd8b0abf3ebcca60260b9f2a5c
Sodaengdong <oisujin98@gmail.com>
2026-01-06 16:59:21 +0900

    commit3_1

--- playwright_3.py
+++ playwright_3.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 # Playwright 실행
 
-with sync_playwright() as p :
+with sync_playwright() as p :  # Playwright 실행과 종료를 자동으로 관리하는 컨텍스트 문
     # Firefox 브라우저 시작
     browser = p.firefox.launch(headless=False, slow_mo=100) # slow_mo : 각 Playwright 동작 사이마다 100밀리초(ms)씩 지연, 눈으로 확인 가능
     page = browser.new_page()
