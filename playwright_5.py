cfa282a8a785197c7a0b9f17fa1093c5f181651a
Sodaengdong <oisujin98@gmail.com>
2026-01-08 17:06:55 +0900

    네이버창에 검색어 입력 후 검색

--- playwright_5.py
+++ playwright_5.py
@@ -1,2 +1,18 @@
 # 버튼 클릭, 텍스트 입력, 드롭다운 선택 등 테스트 작성하기
-# 특정 버튼이 페이지에 존재하는지, 입력 필드가 활성화 상태인지 확인하는 테스트 작성하기
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+with sync_playwright() as p :
+    browser = p.chromium.launch(headless=False, slow_mo=300)
+    page = browser.new_page()
+    
+    page.goto("https://www.naver.com/")
+    
+    #print(page.locator("div#inputWrapper > input#input.truncate").count()) # count()로 선택자 존재 확인하기
+    page.locator("input#query").click()
+    page.locator("input#query").fill("playwright 공부")
+    page.locator("svg#search-btn").click()
+    
+    page.wait_for_timeout(5000)
+    # page.wait_for_selector("text=playwright") #playwright이란 텍스트가 보일 때 까지 기다리기
+    
+    browser.close()
