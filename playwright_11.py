4fd5ee795a9c1d38fc77aafae6eaf37c766f66e9
Sodaengdong <oisujin98@gmail.com>
2026-01-14 01:03:46 +0900

    스크린샷 캡처

--- playwright_11.py
+++ playwright_11.py
@@ -1,1 +1,13 @@
-# 스크린샷 & 콘솔로그 캡처
+# 스크린샷 캡처
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+with sync_playwright() as p :
+    
+    browser = p.chromium.launch(headless=False, slow_mo=300)
+    page = browser.new_page()
+    
+    page.goto("https://the-internet.herokuapp.com/")
+    page.screenshot(path="screenshot.png") # 페이지 전체
+    
+    browser.close()
